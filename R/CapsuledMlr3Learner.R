0ec89eb7a58f3a4885e96d0f3c2697d0d78ff7ef
mb706 <mb706@users.noreply.github.com>
2021-05-16 20:20:49 +0200

    property translate fix

--- R/CapsuledMlr3Learner.R
+++ R/CapsuledMlr3Learner.R
@@ -7,7 +7,7 @@ makeCapsuledLearner <- function(encing) {  # nocov start
   checkmate::assertClass(encing, "R6")
 
   property.translate <- list(
-    missings = "missings",
+    missings = "missing",
     weights = "weights",
     importance = "featimp",
     selected_features = character(0),
